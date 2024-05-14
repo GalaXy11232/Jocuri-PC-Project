3684ece7444890c9956de716c1e90bfa9b5d39f8
GalaXy <neamtumatei08@gmail.com>
2024-05-14 22:46:58 +0300

    csgo video placeholder + fornite

--- script.js
+++ script.js
@@ -9,14 +9,34 @@ window.addEventListener("load", () => {
         for (let i = 1; i < 5; i = i + 2) {
             ls[i].style.opacity = 0; ls[i - 1].style.opacity = 0;
             
-            setTimeout(() => {
+            var t = setTimeout(() => {
                 ls[i].innerHTML = [ls[i - 1].innerHTML, ls[i - 1].innerHTML = ls[i].innerHTML][0];
                 ls[i].style.opacity = 1; ls[i - 1].style.opacity = 1;
             }, 500)
         }
 
     }, 7000)
+
+    
+    const vidDiv = document.getElementById("vid"); 
+    const vidHands = document.getElementById("vid-hands");
+    const vidContainer = document.getElementById("vid-container");
+    const video = document.getElementById("video");
+    var vidActive = false;
+
+    vidHands.style.marginLeft = "-1050px";
+    vidDiv.addEventListener("mouseenter", (ev) => {
+        if (!vidActive) {
+            video.autoplay = true; video.load();
+            vidActive = true;
+        }
+        vidHands.style.marginLeft = "0px"; 
+    })
+
+    vidContainer.addEventListener("mouseleave", (ev) => {vidHands.style.marginLeft = "-1050px";}) // sau video autoplay on mouse enter si onpause sau onend -> mouseleave
+    video.addEventListener("ended", (ev) => {vidActive = false; vidHands.style.marginLeft = "-1050px";})
 })
+
 
 function goto(link) {
     window.location.href = link;
