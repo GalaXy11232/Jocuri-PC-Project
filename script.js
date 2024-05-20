33c9279590ebcf8203ca848779ae934d0c3e1512
GalaXy <neamtumatei08@gmail.com>
2024-05-20 22:43:29 +0300

    ba avem soundboard CU IMAGINI

--- script.js
+++ script.js
@@ -45,18 +45,17 @@ window.addEventListener("load", () => {
 })
 
 var soundboard = ["1", "2", "3"];
-var audio;
 
 document.addEventListener("keypress", (ev) => {
 
     if (ev.key == soundboard[0]) 
-        playAudio("Audios/Vine-boom-sound-effect.mp3", 0.5);
+        playAudio("Audios/Vine-boom-sound-effect.mp3", 0.5, 1);
 
     if (ev.key == soundboard[1]) 
-        playAudio("Audios/extremely-loud-incorrect-buzzer.mp3", 0.25);
+        playAudio("Audios/extremely-loud-incorrect-buzzer.mp3", 0.25, 2);
 
     if (ev.key == soundboard[2]) 
-        playAudio("Audios/correct.mp3", 0.5);
+        playAudio("Audios/correct.mp3", 0.5, 3);
 })
 
 
@@ -64,9 +63,40 @@ function goto(link) {
     window.location.href = link;
 }
 
-function playAudio(aud, vol) {
-    audio = new Audio(aud);
+function playAudio(aud, vol, audID) {
+    var audio = new Audio(aud);
     audio.volume = vol;
     audio.load();
     audio.play();
+
+    soundImage(audID);
+}
+
+function soundImage(soundID) {
+    var image = document.createElement("img");
+    image.width = window.innerWidth - 200; image.height = window.innerHeight;
+    image.style.cssText = `
+        position: absolute;
+        top: ${-1 * document.body.getBoundingClientRect().top + 25 /*de ce 25 nu stiu ca altfel nu intra tot*/};
+        opacity: 1;
+        margin: auto;
+        left: 0;
+        right: 0;
+        text-align: center;
+    `;
+
+    if (soundID == 1) image.src = "https://emojiisland.com/cdn/shop/products/7_large.png?v=1571606116";
+    if (soundID == 2) image.src = "https://static-00.iconduck.com/assets.00/cross-mark-emoji-1019x1024-8iz17ya4.png";
+    if (soundID == 3) image.src = "https://assets.streamlinehq.com/image/private/w_200,h_200,ar_1/f_auto/v1/icons/kawaii-emoji/-symbols/-symbols/u+2714-relcbqqxhecnppvjy5qw8i.png?_a=DAJFJtWIZAAC";
+
+    
+    document.body.appendChild(image);
+
+    let i = setInterval(() => {
+        image.style.opacity = parseFloat(image.style.opacity) - 0.05;
+        if (parseFloat(image.style.opacity) < 0) {
+            clearInterval(i);
+            document.body.removeChild(image);
+        }
+    }, 75)
 }
